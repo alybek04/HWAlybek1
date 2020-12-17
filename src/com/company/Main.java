cca63144957bbcc7b8d4f148a2f44240749a3ac4
Omurbek1048 <seitkulovomurbek@gmail.com>
2020-12-17 23:17:31 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String GeekTech  = "GeekTech";
+        int num = 40;
+        String word = "word";
+        System.out.println(GeekTech + " " + num + " " + word);
+
+
+        if (num > 0);
+        System.out.println("Вы сохранили положительное число");
+
+
+
+
+
+    }
+}
